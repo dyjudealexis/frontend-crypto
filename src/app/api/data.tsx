4f60ccc0db97dd362b509c619cde55806723f6ca
Jude Alexis Dy <dyjudealexis@gmail.com>
2025-05-28 12:42:03 +0800

    update data

--- src/app/api/data.tsx
+++ src/app/api/data.tsx
@@ -21,7 +21,7 @@ export const pricedeta: {
   {
     title: "Bitcoin",
     short: "BTC/USD",
-    icon: "/images/icons/icon-bitcoin.svg",
+    icon: "images/icons/icon-bitcoin.svg",
     background: "bg-warning bg-opacity-20",
     price: "$93,291.24",
     mark: "$94,040.99 (-0.9%)",
@@ -32,7 +32,7 @@ export const pricedeta: {
   {
     title: "Ethereum",
     short: "ETH/USD",
-    icon: "/images/icons/icon-ethereum.svg",
+    icon: "images/icons/icon-ethereum.svg",
     background: "bg-light_grey",
     price: "$3,128.84",
     mark: "$4,878.26 (-35.9%)",
@@ -43,7 +43,7 @@ export const pricedeta: {
   {
     title: "Bitcoin Cash ",
     short: "BTC/USD",
-    icon: "/images/icons/icon-bitcoin-circle.svg",
+    icon: "images/icons/icon-bitcoin-circle.svg",
     background: "bg-warning bg-opacity-20",
     price: "$443.27",
     mark: "$3,785.82 (-88.3%)",
@@ -54,7 +54,7 @@ export const pricedeta: {
   {
     title: "Litecoin",
     short: "LTC/USD",
-    icon: "/images/icons/icon-litecoin.svg",
+    icon: "images/icons/icon-litecoin.svg",
     background: "bg-light_grey",
     price: "$86.11",
     mark: "$410.26 (-79.1%)",
@@ -65,7 +65,7 @@ export const pricedeta: {
   {
     title: "Solana",
     short: "SOL/USD",
-    icon: "/images/icons/icon-solana.svg",
+    icon: "images/icons/icon-solana.svg",
     background: "bg-light_grey",
     price: "$238.70",
     mark: "$259.96 (-8.2%)",
@@ -76,7 +76,7 @@ export const pricedeta: {
   {
     title: "Dogecoin",
     short: "DOGE/USD",
-    icon: "/images/icons/icon-dogecoin.svg",
+    icon: "images/icons/icon-dogecoin.svg",
     background: "bg-light_grey",
     price: "$0.394",
     mark: "$0.7316 (-46.2%)",
@@ -88,15 +88,15 @@ export const pricedeta: {
 
 export const portfolioData: { image: string; title: string }[] = [
   {
-    image: "/images/portfolio/icon-wallet.svg",
+    image: "images/portfolio/icon-wallet.svg",
     title: "Track your investments",
   },
   {
-    image: "/images/portfolio/icon-vault.svg",
+    image: "images/portfolio/icon-vault.svg",
     title: "Secure asset storage",
   },
   {
-    image: "/images/portfolio/icon-mobileapp.svg",
+    image: "images/portfolio/icon-mobileapp.svg",
     title: "Access on the go",
   },
 ];
@@ -115,19 +115,19 @@ export const perksData: {
   space: string;
 }[] = [
   {
-    icon: "/images/perks/icon-support.svg",
+    icon: "images/perks/icon-support.svg",
     title: "Round-the-Clock Help",
     text: "Have questions? Our expert team is here anytime you need.",
     space: "lg:mt-8",
   },
   {
-    icon: "/images/perks/icon-community.svg",
+    icon: "images/perks/icon-community.svg",
     title: "Global Network",
     text: "Connect with users in our vibrant global community",
     space: "lg:mt-14",
   },
   {
-    icon: "/images/perks/icon-academy.svg",
+    icon: "images/perks/icon-academy.svg",
     title: "Learning Center",
     text: "Explore blockchain and<br /> crypto essentials at no cost.",
     space: "lg:mt-4",
@@ -141,25 +141,25 @@ export const timelineData: {
   position: string;
 }[] = [
   {
-    icon: "/images/timeline/icon-planning.svg",
+    icon: "images/timeline/icon-planning.svg",
     title: "Initial Strategy",
     text: "Define goals, scope, and technical foundation",
     position: "md:top-0 md:left-0",
   },
   {
-    icon: "/images/timeline/icon-refinement.svg",
+    icon: "images/timeline/icon-refinement.svg",
     title: "Enhancement",
     text: "Polish and optimize every detail of the plan",
     position: "md:top-0 md:right-0",
   },
   {
-    icon: "/images/timeline/icon-prototype.svg",
+    icon: "images/timeline/icon-prototype.svg",
     title: "Demo Creation",
     text: "Craft a demo version to gather early feedback",
     position: "md:bottom-0 md:left-0",
   },
   {
-    icon: "/images/timeline/icon-support.svg",
+    icon: "images/timeline/icon-support.svg",
     title: "Ongoing Care",
     text: "Launch the product and continue proactive assistance",
     position: "md:bottom-0 md:right-0",
